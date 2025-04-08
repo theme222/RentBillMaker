28703e7986a00ebacdb0628803b930dd0bf8cebb
theme222 <sira.tongsima@yahoo.com>
2025-04-08 11:08:55 +0700

    Whoops

---
+++ app/components/Background.tsx
@@ -0,0 +1,4 @@
+export default function Background() 
+{
+    return <div className="min-w-screen min-h-screen bg-base-100 fixed z-[-20]"></div>;
+}
---
+++ app/routes.ts
@@ -0,0 +1,13 @@
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+
+export default 
+[
+    route("/", "./routes/Home.tsx"),
+] satisfies RouteConfig;
+
+
+
+
+
+
+
